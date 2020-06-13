1f1112e1da34d327ea7b63668d372c81d0c3497e
Kadir Tugan <kadir.tugan@careem.com>
2020-06-13 12:27:02 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module loci
+
+go 1.14
+
+require (
+	github.com/Microsoft/hcsshim v0.8.9 // indirect
+	github.com/containerd/containerd v1.3.4 // indirect
+	github.com/containerd/continuity v0.0.0-20200413184840-d3ef23f19fbb // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v17.12.0-ce-rc1.0.20200531234253-77e06fda0c94+incompatible
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/opencontainers/runc v0.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	google.golang.org/grpc v1.29.1 // indirect
+)
---
+++ loci.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/archive"
+	"github.com/docker/docker/pkg/stdcopy"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+//need commands
+//build: to check if the docker build is outdated
+//invoke command
+
+// todo: feature - immutable volumes
+// todo: feature - gitlab ci parser?
+// todo: env variables
+
+func tagFromDockerfile(dockerFilePath string) string {
+	dockerFilePath, err := filepath.Abs(dockerFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	tag := strings.ReplaceAll(dockerFilePath, "/", "_")
+	tag = strings.ToLower(tag)
+	tag = strings.Trim(tag, "_")
+	return tag
+}
+
+
+
+func BuildImage(cli *client.Client, dockerFilePath string) string {
+	if dockerFilePath == "" {
+		log.Fatal("Path to Dockerfile cannot be empty")
+	}
+
+	dockerFilePath, err := filepath.Abs(dockerFilePath)
+	tag := tagFromDockerfile(dockerFilePath)
+
+	base, filename := filepath.Split(dockerFilePath)
+
+	ctx, _ := archive.TarWithOptions(base, &archive.TarOptions{})
+	resp, err := cli.ImageBuild(context.Background(), ctx, types.ImageBuildOptions{
+		Dockerfile: filename,
+		Tags: []string{tag},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	newStr := buf.String()
+
+	fmt.Printf(newStr)
+	return tag
+}
+
+type config struct {
+	BuildFolder		string
+
+}
+
+func main() {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	//todo need to build or build image first
+
+	//-------------- PARAMETER PART --------------------
+	image := ""
+	buildFlag := false
+	pDockerfile := "/home/kadir/workspace/kubernetes/docker-automation/Dockerfile"
+	if buildFlag {
+		image = BuildImage(cli, pDockerfile)
+	}else{
+		image = tagFromDockerfile(pDockerfile)
+	}
+
+	// ----------- END PARAMETER PART --------
+	if image == ""  {
+		log.Fatal("No tag given") // todo probably just temporary and needs to be put into the parser.
+	}
+
+	cwd, err := filepath.Abs(".")
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := cli.ContainerCreate(context.Background(), &container.Config{
+		Image: image,
+		Cmd:   []string{"bash", "-c", "ls"},
+		WorkingDir: "/build",
+	}, &container.HostConfig{
+		Mounts: []mount.Mount{
+			mount.Mount{
+				Type: mount.TypeBind,
+				Source: "/home/kadir/.aws",
+				Target: "/root/.ssh",
+			},
+			mount.Mount{
+				Type:   mount.TypeBind,
+				Source: cwd,
+				Target: "/build",
+			},
+		},
+	}, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	err = cli.ContainerStart(context.Background(), resp.ID, types.ContainerStartOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	out, err := cli.ContainerLogs(context.Background(), resp.ID, types.ContainerLogsOptions{
+		Follow: true, ShowStdout: true, ShowStderr: true, Tail: "all",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+
+	//fmt.Printf(resp.ID, resp.Warnings)
+
+}
+
